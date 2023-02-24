23a2c95321b9c1ecc75324ca7c9d1da6c2f87fe7
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-24 13:33:34 +0100

    принимает на вход число N и выдаёт произведение чисел от 1 до N

---
+++ Seminar004_2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*28 Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.*/
+
+Console.WriteLine("Enter number: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Multiplication of {n} = {Mult (n)}");
+
+int Mult (int n)
+{
+    int mult = 1;
+    int s = 2;
+    if(n < 0)
+    s = n;// нужно s<=n, n= -5, 2<=-5 -цикл не запустится 
+        //никогда,если не поменять местами s и n. s=n -> -5=-1
+    n = -1;
+    for(; s <= n; s++ )
+    {
+        mult = mult * s;
+    }
+    return mult;
+}
+
+
