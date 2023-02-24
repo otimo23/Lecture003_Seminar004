506b5a04815edad48e1a8b08a63e3c5fea17b590
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-24 13:29:58 +0100

    на вход принимает число N и выводит сумму от 1 до N

---
+++ Seminar004/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая на вход принимает
+ число N и выводит сумму от 1 до N.*/
+
+int GetSum(int n)
+{ 
+    int sum = 0;
+    for(int i = 1; i <= n; i++)
+        sum+=i; //sum = sum +i
+    return sum;
+}
+Console.Clear();
+Console.Write("Enter number: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Summ from 1 to {n} is {GetSum(n)}");
