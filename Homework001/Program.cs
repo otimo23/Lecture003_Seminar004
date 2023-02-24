f514ac2324c195d85f71f777f1e2691ed3bc32ba
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-24 13:37:43 +0100

    принимает на вход два числа (A и B) и возводи число A в натуральную степень B

---
+++ Homework001/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит
+ число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.WriteLine("Enter A:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter B:");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write($"A in power B {GetPower(a,b)}");
+
+int GetPower(int c, int d)
+{
+    int result = 1;
+    for(int i = 1; i <= d; i++)
+    result = result * c;
+    return result;
+}
+
+
+
