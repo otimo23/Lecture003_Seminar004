189d18f1c16958f19d86fe7bd8e0a36f538940fd
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-24 13:39:11 +0100

    Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе

---
+++ Homework002/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+Console.WriteLine("Enter number ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Sum of digits {GetSum(number)}");
+
+int GetSum(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    // как работает последняя цифра?
+    {
+        sum+= n%10;sum=sum+(n%10)
+        n = n/10;
+    }
+    return sum;
+}
