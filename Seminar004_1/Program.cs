3b7431e38e057a09224100d76e749df72e5e1b0a
olga Timoshenko <otimoshenko55@gmail.com>
2023-02-24 13:31:53 +0100

    принимает на вход число и выдаёт количество цифр в числе

---
+++ Seminar004_1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*26 Напишите программу,
+//  которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5*/
+
+Console.WriteLine("Enter number: ");
+int s = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Digits number is {GetNum(s)}");
+
+int GetNum (int n)
+{
+    int count = 0;
+    if (n < 0)
+    n = n * -1;
+    while (n > 0)
+    {
+        n/=10;
+        count++;
+    }
+    return count;
+}
